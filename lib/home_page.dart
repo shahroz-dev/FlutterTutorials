279d85dcbcef274fe7049d44d48621a919ad4396
Muhammad Shahroz <shahrozarif28@gmail.com>
2023-04-23 18:08:18 +0500

    second commit

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  final days = 30;
+  final name = "codepur";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalog App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'home_page.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -9,13 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Material(
-      child: Center(
-        child: Container(
-          child: const Text("Welcome to 30 days of flutter"),
-        ),
-      ),
-    ));
+    int days = 30;
+    String name = "Codepur";
+
+    return MaterialApp(home: HomePage());
   }
 }
