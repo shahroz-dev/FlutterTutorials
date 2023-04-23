d6531fb45d65b4500272a969813d75f94e4944f5
Muhammad Shahroz <shahrozarif28@gmail.com>
2023-04-23 21:55:17 +0500

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertutorials/Pages/login_page.dart';
 
-import 'home_page.dart';
+import 'Pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,11 +10,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  @override
+  @override //override means is to inherite this method from base class for their own purpose
   Widget build(BuildContext context) {
     int days = 30;
     String name = "Codepur";
 
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => const LoginPage(),
+        "/home": (context) => const HomePage(),
+        "/login": (context) => const LoginPage(),
+      },
+    );
   }
 }
